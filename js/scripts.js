e168bbe9a7900acf155648ef34e8d9b9156d5c66
Amit Mhatre & Kellen Kolbeck <student@epicodus.com>
2015-09-10 17:33:29 -0700

    work in progress

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+var romanNumerals = function(number) {
+
+  var romanNum = new symbolNum();
+  romanNum['I'] = 1;
+  romanNum['V'] = 5;
+  romanNum['X'] = 10;
+  romanNum['L'] = 50;
+  romanNum['C'] = 100;
+  romanNum['D'] = 500;
+  romanNum['M'] = 1000;
+
+  var num = number.toString();
+
+  num_array = num.split("");
+
+  num_array = num_array.reverse();
+
+  rom_unit = []; rom_tens = []; rom_hund =[]; rom_thou =[];
+
+  if (num_array.length >= 1) {
+    var unit = num_array[0]
+      if (unit === 9) {
+        rom_unit.push("IX")
+      }
+
+
+
+      }
+
+    }
+
+  }
+
+  if ((num_array.length === 2) {
+    var tens = num_array[1]
+  }
+
+  if (num_array.length === 3) {
+    var hundreds = num_array[2]
+  }
+
+
+
+
+
+
+
+  // convert number to string
+
+  // split()
+
+  //
+
+  //  separate digits in 100s, 10s, units etc.....
+
+  if (number
+
+
+
+
+
+
+
+
+
+
+}
+
+
+// output = "";
+// roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+// nums = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+//
+// for (var i = 0; i <nums.length; i++) {
+//
+//   if (number >= nums[i];
+//     output += roman[i];
+//     number -= nums[i];
+//     i=0;
+//   }
+// }
+//
+// }
+// return output;
---
+++ spec/specs.js
@@ -0,0 +1,39 @@
+describe('romanNumerals', function(){
+
+  it("it returns the value of the number in roman numerals", function(){
+    expect(romanNumerals(3)).to.equal("III");
+  });
+
+  // it("it returns the value of the number in roman numerals", function(){
+  //   expect(romanNumerals(4)).to.equal("IV");
+  // });
+  //
+  // it("it returns the value of the number in roman numerals", function(){
+  //   expect(romanNumerals(12)).to.equal("XII");
+  // });
+  //
+
+
+
+  //
+  // it("it outputs list of numbers up to the dividend ", function(){
+  //   expect(countUp(4,22)).to.eql([4, 8, 12, 16, 20]);
+  // });
+  //
+  // it("it outputs list of numbers up to the dividend ", function(){
+  //   expect(countUp(3,6)).to.eql([3, 6]);
+  // });
+
+  // it("it is isoceles if any two sides are equal", function(){
+  //   expect(triangleTracker(3,3,5)).to.equal("isoceles");
+  // });
+  //
+  // it("it is scalene if none of the sides are equal", function(){
+  //   expect(triangleTracker(2,4,5)).to.equal("scalene");
+  // });
+  //
+  // it("it is not a triangle if the sum of any two sides is smaller the the third side", function(){
+  //   expect(triangleTracker(6,12,5)).to.equal("this is not a triangle!");
+  // });
+  //
+});
